bd8acc120d0052b8cae8cffd982e664400522345
Matheus Vigaro <matheus@vigaro.tk>
2015-05-02 10:32:35 -0300

    Add light wand

--- src/java/tk/vigaro/justanothermod/ModInformation.java
+++
@@ -1,7 +0,0 @@
-package tk.vigaro.justanothermod;
-
-public class ModInformation {
-	public static final String ID = "justanothermod";
-	public static final String NAME = "Just Another Mod";
-	public static final String VERSION = "0.0.0";
-}
